fcb68dd5b3f4d46b4d35cbd2af9e06866816ba1e
Murphy Stroppa <murph33@gmail.com>
2015-07-03 12:34:34 -0700

    Added ajax functionality to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,24 +8,31 @@ class CommentsController < ApplicationController
 
   def create
     comment_params = params.require(:comment).permit(:body)
-    discussion = Discussion.find params[:discussion_id]
-    project = Project.find discussion.project_id
+    @discussion = Discussion.find params[:discussion_id]
+    project = Project.find @discussion.project_id
     @comment = Comment.new comment_params
     @comment.user = current_user
-    @comment.discussion = discussion
-    if @comment.save
-      DiscussionsMailer.delay.notify_discussion_owner(discussion) unless @comment.user == discussion.user
-      flash[:notice] = "Comment successfully posted!"
-      redirect_to project_discussion_path(project, discussion)
-    else
-      flash[:alert] = "Something went wrong!"
-      redirect_to project_discussion_path(project, discussion)
+    @comment.discussion = @discussion
+    respond_to do |format|
+      if @comment.save
+        DiscussionsMailer.delay.notify_discussion_owner(@discussion) unless @comment.user == @discussion.user
+        format.js { render }
+        format.html { flash[:notice] = "Comment successfully posted!"
+        redirect_to project_discussion_path(project, @discussion) }
+        else
+        format.js { render "create_failure" }
+        format.html { flash[:alert] = "Something went wrong!"
+        redirect_to project_discussion_path(project, @discussion) }
+      end
     end
   end
 
   def edit
     @comment = Comment.find params[:id]
     @discussion = Discussion.find @comment.discussion_id
+    respond_to do |format|
+      format.js { render }
+    end
   end
 
   def update
@@ -33,22 +40,29 @@ class CommentsController < ApplicationController
     @comment = Comment.find params[:id]
     @discussion = Discussion.find params[:discussion_id]
     project = Project.find @discussion.project_id
-    if @comment.update comment_params
-      flash[:notice] = "Comment updated!"
-      redirect_to project_discussion_path(project, @discussion)
-    else
-      flash[:alert] = "Something went wrong!"
-      render "/comments/edit"
+    respond_to do |format|
+      if @comment.update comment_params
+        format.js   { render }
+        format.html { flash[:notice] = "Comment updated!"
+                    redirect_to project_discussion_path(project, @discussion) }
+        else
+        format.js   { render }
+        format.html { flash[:alert] = "Something went wrong!"
+                    render "/comments/edit" }
+      end
     end
   end
 
   def destroy
-    comment = Comment.find params[:id]
-    @discussion = Discussion.find comment.discussion_id
+    @comment = Comment.find params[:id]
+    @discussion = Discussion.find @comment.discussion_id
     @project = Project.find @discussion.project_id
-    comment.destroy
-    flash[:notice] = "Comment successfully deleted."
-    redirect_to project_discussion_path(@project, @discussion)
+    @comment.destroy
+    respond_to do |format|
+      format.js { render }
+      format.html { flash[:notice] = "Comment successfully deleted."
+                    redirect_to project_discussion_path(@project, @discussion) }
+    end
   end
 
 end
